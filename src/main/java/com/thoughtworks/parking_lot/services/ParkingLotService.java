16801a7618085193ae3315d6f25175ae3b7c195b
jed1337 <j.caychingco@yahoo.com>
2019-10-20 15:45:14 +0800

    Added delete parking lot tests

--- src/main/java/com/thoughtworks/parking_lot/services/ParkingLotService.java
+++ src/main/java/com/thoughtworks/parking_lot/services/ParkingLotService.java
@@ -5,13 +5,25 @@ import com.thoughtworks.parking_lot.repositories.ParkingLotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ParkingLotService {
     @Autowired
     private ParkingLotRepository parkingLotRepository;
 
-
     public ParkingLot save(ParkingLot parkingLot) {
         return parkingLotRepository.save(parkingLot);
+    }
+
+    public boolean delete(String parkingLotName) {
+        Optional<ParkingLot> optionalParkingLot = parkingLotRepository.findById(parkingLotName);
+
+        if (optionalParkingLot.isPresent()) {
+            ParkingLot foundParkingLot = optionalParkingLot.get();
+            parkingLotRepository.delete(foundParkingLot);
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/thoughtworks/parking_lot/services/ParkingLotServieTest.java
+++ src/test/java/com/thoughtworks/parking_lot/services/ParkingLotServieTest.java
@@ -3,10 +3,13 @@ package com.thoughtworks.parking_lot.services;
 import com.thoughtworks.parking_lot.entities.ParkingLot;
 import com.thoughtworks.parking_lot.repositories.ParkingLotRepository;
 import org.hamcrest.MatcherAssert;
+import org.hibernate.validator.internal.constraintvalidators.bv.AssertTrueValidator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -34,5 +37,23 @@ public class ParkingLotServieTest {
         assertThat(savedParkingLot.getName(), is("my parking lot"));
         assertThat(savedParkingLot.getCapacity(), is(30));
         assertThat(savedParkingLot.getLocation(), is("Manila"));
+    }
+
+    @Test
+    public void should_delete_existing_parkingLot() {
+        String parkingLotName = "my parking lot";
+        ParkingLot parkingLot = new ParkingLot(parkingLotName);
+        when(parkingLotRepository.findById(parkingLotName)).thenReturn(Optional.of(parkingLot));
+
+        boolean wasDeleted = parkingLotService.delete(parkingLotName);
+
+        assertThat(wasDeleted, is(true));
+    }
+
+    @Test
+    public void should_not_delete_non_existing_parkingLot() {
+        boolean wasDeleted = parkingLotService.delete("invalid parking lot");
+
+        assertThat(wasDeleted, is(false));
     }
 }
