6513c004a5202ee24bdfa74578d0918102173807
jed1337 <j.caychingco@yahoo.com>
2019-10-20 13:46:25 +0800

    Corrected test annotations in order service test

--- src/test/java/com/thoughtworks/parking_lot/services/OrderServiceTest.java
+++ src/test/java/com/thoughtworks/parking_lot/services/OrderServiceTest.java
@@ -3,27 +3,22 @@ package com.thoughtworks.parking_lot.services;
 import com.thoughtworks.parking_lot.entities.Order;
 import com.thoughtworks.parking_lot.repositories.OrderRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-//@ExtendWith(SpringExtension.class)
-//@ActiveProfiles(profiles = "test")
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderServiceTest {
-    @MockBean
-    private OrderRepository orderRepository;
-
     @Autowired
     private OrderService orderService;
+
+    @MockBean
+    private OrderRepository orderRepository;
 
     @Test
     public void should_save_new_order() {
